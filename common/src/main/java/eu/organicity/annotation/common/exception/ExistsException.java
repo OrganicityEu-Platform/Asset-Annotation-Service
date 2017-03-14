e1210e182d8f59c5a1232564f8bef27233818b83
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2017-03-14 14:03:29 +0200

    add exceptions here

---
+++ common/src/main/java/eu/organicity/annotation/common/exception/ExistsException.java
@@ -0,0 +1,8 @@
+package eu.organicity.annotation.common.exception;
+
+
+public class ExistsException extends Exception {
+    public ExistsException(String string) {
+        super(string);
+    }
+}
---
+++ common/src/main/java/eu/organicity/annotation/common/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package eu.organicity.annotation.common.exception;
+
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String string) {
+        super(string);
+    }
+}
