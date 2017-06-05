bcd91e816616eeff6765aa6cfca7b4843afb1dcf
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2017-06-05 18:58:33 +0300

    dto uses a Long instead of long

--- common/src/main/java/eu/organicity/annotation/common/dto/TagDomainDTO.java
+++ common/src/main/java/eu/organicity/annotation/common/dto/TagDomainDTO.java
@@ -8,7 +8,7 @@ import java.util.Set;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TagDomainDTO {
-    private long id;
+    private Long id;
     private String urn;
     private String description;
     private String user;
@@ -16,61 +16,61 @@ public class TagDomainDTO {
     private Set<ServiceDTO> services;
     private Long created;
     private Long modified;
-
+    
     public TagDomainDTO() {
     }
-
-    public TagDomainDTO(long id, String urn, String description, Set<TagDTO> tags) {
+    
+    public TagDomainDTO(Long id, String urn, String description, Set<TagDTO> tags) {
         this.id = id;
         this.urn = urn;
         this.description = description;
         this.tags = tags;
     }
-
-    public long getId() {
+    
+    public Long getId() {
         return id;
     }
-
-    public void setId(long id) {
+    
+    public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getUrn() {
         return urn;
     }
-
+    
     public void setUrn(String urn) {
         this.urn = urn;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
     public Set<TagDTO> getTags() {
         return tags;
     }
-
+    
     public void setTags(Set<TagDTO> tags) {
         this.tags = tags;
     }
-
+    
     public Set<ServiceDTO> getServices() {
         return services;
     }
-
+    
     public void setServices(Set<ServiceDTO> services) {
         this.services = services;
     }
-
+    
     public String getUser() {
         return user;
     }
-
+    
     public void setUser(String user) {
         this.user = user;
     }
