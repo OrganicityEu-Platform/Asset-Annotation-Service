4f7e1169b15b21dd4bdd14965714be1ce684b16f
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2017-06-08 12:38:38 +0300

    actually post the generated annotations to the Orion

--- service/src/main/java/eu/organicity/annotation/service/OrionService.java
+++ service/src/main/java/eu/organicity/annotation/service/OrionService.java
@@ -6,9 +6,13 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,9 +20,15 @@ import java.util.Set;
 @Service
 public class OrionService {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrionService.class);
-
+    
     @Autowired
     TaggingRepository taggingRepository;
+    private RestTemplate restClient;
+    
+    @PostConstruct
+    public void init() {
+        restClient = new RestTemplate();
+    }
     
     @Async
     public void updateAnnotations(final String assetUrn) {
@@ -27,11 +37,27 @@ public class OrionService {
         final List<Tagging> taggings = taggingRepository.findByUrn(assetUrn);
         for (final Tagging tagging : taggings) {
             allTags.add(tagging.getTag().getUrn());
+            allTags.add(tagging.getTag().getName());
         }
         
         final String tagsString = StringUtils.join(allTags, ",");
         
         LOGGER.info("[" + assetUrn + "] " + tagsString);
+        try {
+            postAnnotationsToOrion(assetUrn, tagsString);
+        } catch (Exception e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
+        }
         
     }
+    
+    private void postAnnotationsToOrion(String assetUrn, String tagsString) {
+        LinkedMultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        headers.add("Fiware-Service", "organicity");
+        headers.add("Fiware-ServicePath", "/");
+        HttpEntity<String> request = new HttpEntity<>("{\"annotations\":{\"value\":\"" + tagsString + "\",\"type\":\"urn:oc:attributeType:annotations\"}}", headers);
+        restClient.postForEntity("http://localhost:1026/v2/entities/" + assetUrn + "/attrs", request, String.class);
+    }
+    
 }
